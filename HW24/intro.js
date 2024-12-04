e3f61dea74772397da2dc88694af6f9e3f55eae3
UmeRoman33 <ume.roman33@gmail.com>
2024-12-05 00:05:37 +0500

    Add files via upload

---
+++ HW24/intro.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    var A=12;
+    document.getElementById("A").innerHTML= A * A; 
+} 
+
+function function2(){
+    var X=5;
+    document.getElementById("B").innerHTML= X * X * X;  
+}
